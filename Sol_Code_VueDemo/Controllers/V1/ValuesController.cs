7b4de4ca27a892cf05262d09f53e69f2bc63a2e9
majinhui <majhoff2013@sin.cn>
2019-04-26 15:21:47 +0800

    修改--version1.1

--- Sol_Code_VueDemo/Controllers/V1/ValuesController.cs
+++ Sol_Code_VueDemo/Controllers/V1/ValuesController.cs
@@ -5,9 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Sol_Code_VueDemo.Controllers
+namespace Sol_Code_VueDemo.Controllers.V1
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -23,6 +24,24 @@ namespace Sol_Code_VueDemo.Controllers
         public string Get(int id)
         {
             return "value";
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
--- Sol_Code_VueDemo/Startup.cs
+++ Sol_Code_VueDemo/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Sol_Code_VueDemo
 {
@@ -26,6 +28,39 @@ namespace Sol_Code_VueDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            #region Versioning
+            services.AddApiVersioning(o =>
+            {
+                o.ReportApiVersions = true;//return versions in a response header
+                o.DefaultApiVersion = new ApiVersion(1, 0);//default version select 
+                o.AssumeDefaultVersionWhenUnspecified = true;//if not specifying an api version,show the default version
+            });
+            #endregion
+
+            #region swagger
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Contact = new Contact
+                    {
+                        Name = "Demo MaJh",
+                        Email = "519610726.com",
+                        Url = "https://baidu.com"
+                    },
+                    Description = "A front-background project build by ASP.NET Core 2.1 and Vue",
+                    Title = "Sol_Code_VueDemo",
+                    Version = "v1"
+                });
+
+                var basePath = Path.GetDirectoryName(AppContext.BaseDirectory);//get application located directory
+                var apiPath = Path.Combine(basePath, "Sol_Code_VueDemo.xml");
+                var dtoPath = Path.Combine(basePath, "TestDemo.Application.xml");
+                s.IncludeXmlComments(apiPath, true);
+                s.IncludeXmlComments(dtoPath, true);
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +78,12 @@ namespace Sol_Code_VueDemo
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Sol_Code_VueDemo API V1.0");
+            });
         }
     }
 }
